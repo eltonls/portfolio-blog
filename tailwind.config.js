e379677a4a121043db40c5ba28e73c5be03250a9
Elton <elton_dev@outlook.com>
2023-08-07 12:08:31 -0300

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'foreground': '#9D8DF1', // Foreground
+        'background': '#1E1E2E', // Backgrounds
+        'backgroundtwo': '#181825', // BG-2
+      },
+      fontFamily: {
+        sans: ['Glory', 'sans-serif'],
+        serif: ['Cormorant Garamond', 'serif']
+      }
+    },
+  },
+  plugins: [],
+}
+
