db31cf19eb5cc98c2bdf690d9656fd8890974d60
Elton <eltonrs1999@gmail.com>
2023-12-28 17:33:16 -0300

    fix: erros de tipo em typescript

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
-import * as React from "react"
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+import { Routes, Route } from "react-router-dom"
 import Home from "./pages/Home"
 import About from "./pages/About"
 import Projects from "./pages/Projects"
@@ -9,9 +8,9 @@ function App() {
   return (
     <div className="bg-hero-pattern overflow-scroll h-screen w-screen bg-cover bg-repeat overflow-y-scroll bg-center bg-opacity-50 py-5 px-8">
       <Routes>
-        <Route exact path="/" element={<Home />}></Route>
-        <Route exact path="/projects" element={<Projects />}></Route>
-        <Route exact path="/about" element={<About />}></Route>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/projects" element={<Projects />}></Route>
+        <Route path="/about" element={<About />}></Route>
       </Routes>
     </div>
   )
